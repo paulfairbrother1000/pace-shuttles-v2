449801b6443cfc75dc6721ed9ca178ded5bfb80d
paulfairbrother1000 <paul.fairbrother@beyondservicemanagement.com>
2026-08-25 20:56:14 -0400

    fix: remove legacy duplicate seat enhancement

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,6 +1,5 @@
 import CustomerBooking from '@/components/customer-booking';
-import BookingCardEnhancements from '@/components/booking-card-enhancements';
 
 export default function Book(){
-  return <><CustomerBooking/><BookingCardEnhancements/></>;
+  return <CustomerBooking/>;
 }
